c775d7af2d5df6de37512d0b187fb80dda995733
Yun Jung Choi <49920477+yunjchoi@users.noreply.github.com>
2021-11-30 10:58:01 -0800

    Onboard arc appliance to genie (#1355)
    * Create config.json
    
    * Create override.json
    
    * Add files via upload
    
    * Delete applianceextensionsaa.svg
    
    * Add files via upload
    
    * Update version-test.service.ts
    
    Routing Arc Appliance traffic to new genie
    
    * Update config.json

--- AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/version-test.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/version-test.service.ts
@@ -18,7 +18,8 @@ export const allowV3PResourceTypeList: { type: string, allowSwitchBack: boolean 
     { type: "microsoft.appplatform/spring", allowSwitchBack: false },
     { type: "microsoft.containerservice/managedclusters", allowSwitchBack: false },
     { type: "microsoft.web/containerapps", allowSwitchBack: false },
-    { type: "microsoft.servicefabric/clusters", allowSwitchBack: false }
+    { type: "microsoft.servicefabric/clusters", allowSwitchBack: false },
+    { type: "microsoft.resourceconnector/appliances", allowSwitchBack: false }
 ];
 
 
